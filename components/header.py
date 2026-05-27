331ba188812d157e10692b9740206d024acac231
ezekielmose <70557286+ezekielmose@users.noreply.github.com>
2026-05-27 08:55:25 +0300

    Update header.py

--- components/header.py
+++ components/header.py
@@ -1,1 +1,27 @@
+import streamlit as st
 
+# ======================================
+# HEADER COMPONENT (TABS NAVIGATION)
+# ======================================
+def show_header():
+
+    col1, col2 = st.columns([1, 8])
+
+    # ======================================
+    # LOGO
+    # ======================================
+    with col1:
+        st.image("assets/logo.png", width=120)
+
+    # ======================================
+    # TABS MENU
+    # ======================================
+    with col2:
+        tabs = st.tabs([
+            "Home",
+            "AI Analyzer",
+            "Our Services",
+            "About Us"
+        ])
+
+    return tabs
