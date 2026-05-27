e7a75bee5f2e39fedd43413d67525be91b2b2b19
ezekielmose <70557286+ezekielmose@users.noreply.github.com>
2026-05-27 08:56:11 +0300

    Update footer.py

--- components/footer.py
+++ components/footer.py
@@ -1,1 +1,12 @@
+import streamlit as st
 
+# ======================================
+# FOOTER COMPONENT
+# ======================================
+def show_footer():
+
+    st.markdown("""
+    <div class="footer">
+        © 2026 Genesis Digital | All Rights Reserved
+    </div>
+    """, unsafe_allow_html=True)
