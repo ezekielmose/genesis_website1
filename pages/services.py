5bbe48de88f2b18044b090a9959245e40736fdf9
ezekielmose <70557286+ezekielmose@users.noreply.github.com>
2026-05-27 08:50:15 +0300

    Update services.py

--- pages/services.py
+++ pages/services.py
@@ -1,1 +1,19 @@
+import streamlit as st
 
+# ======================================
+# SERVICES PAGE
+# ======================================
+def show_services():
+
+    st.title("Our Services")
+
+    col1, col2, col3 = st.columns(3)
+
+    with col1:
+        st.subheader("Video Sourcing")
+
+    with col2:
+        st.subheader("AI Solutions")
+
+    with col3:
+        st.subheader("Data Analysis")
